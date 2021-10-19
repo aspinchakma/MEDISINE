7c07bc58c88c89d64ffd8550c873bc1f1b85cc3f
aspinchakma <85893728+aspinchakma@users.noreply.github.com>
2021-10-19 10:38:45 +0600

    finished not found page

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Banner.css';
 
 const Banner = () => {
@@ -9,7 +10,8 @@ const Banner = () => {
                     <h3 className="banner-details">Protect Yourself</h3>
                     <h1 className="main-header">Covid-19 Prevention Product Combo</h1>
                     <p className="banner-details">Get the best Grade A safety mask for you & your loved ones protection.</p>
-                    <button className="banner-buy-button">Starts from $0.99</button>
+                    <Link to="details/7"><button className="banner-buy-button">Starts from $0.99</button></Link>
+
                 </div>
                 <div>
                     <img src="http://magento2.magentech.com/themes/sm_medisine/pub/media/wysiwyg/banner/item-6.jpg" alt="" />
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>This is Not found page</h1>
+        <div className="not-found text-center row py-5 my-4">
+            <div className="col-lg-3">
+
+            </div>
+            <div className="col-lg-6 my-5">
+                <h1>4<sapn>0</sapn>4</h1>
+                <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+                <Link to="/home"><button className="back-to-home mt-4">HOME</button></Link>
+            </div>
+            <div className="col-lg-3">
+
+            </div>
         </div>
     );
 };
